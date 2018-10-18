76a473985a8b57e029d0598ac201085034a8b22a
Noelia Vaz <nlvazquez01@gmail.com>
2018-10-18 08:21:04 -0400

    Added Some Quotes to script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,67 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
+var quotes = [
+    {
+        quote: 'It does not do well to dwell on dreams and forget to live.',
+        source: 'J.K. Rowling',
+        citation: 'Harry Potter and the Chamber of Secrets',
+        year: '1998',
+        category:'Life'
+    },
+    {
+        quote: 'Happiness can be found in the darkest of times, if one only remembers to turn on the light.',
+        source: 'Steve Kloves',
+        citation: 'Harry Potter and the Prisoner of Azkaban Film',
+        year: '2004',
+        category: 'Happiness'
+    },
+    {
+        quote: 'Something with Forrest Gump',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    },
+    {
+        quote: '',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    },
+    {
+        quote: '',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    },
+    {
+        quote: '',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    },
+    {
+        quote: '',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    },
+    {
+        quote: '',
+        source: '',
+        citation: '',
+        year: '',
+        category: ''
+    }
+];
 
 
-
-// Create the getRandomQuuote function and name it getRandomQuote
+// Create the getRandomQuote function and name it getRandomQuote
 
 
 
